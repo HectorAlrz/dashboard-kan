80276c14725df3eed634ba09c195c4f26ba71aa5
HectorAlrz <healcaraz@gmail.com>
2024-12-20 19:26:31 -0600

    add: customFetch and errors hanlder

--- src/providers/data/fetch-wrapper.ts
+++ src/providers/data/fetch-wrapper.ts
@@ -1,6 +1,14 @@
-const customFetch = async (url: string, options: RequestInit) => {
+import { GraphQLFormattedError } from "graphql";
+
+type Error = {
+  message: string;
+  statusCode: string
+}
+
+export const customFetch = async (url: string, options: RequestInit) => {
   const accessToken = localStorage.getItem("access_token");
   const headers = options.headers as Record<string, string>;
+
   return await fetch(url, {
     ...options,
     headers: {
@@ -10,4 +18,28 @@ const customFetch = async (url: string, options: RequestInit) => {
       "Apollo-Require-Preflight": "true",
     },
   });
+};
+
+export const getGraphQLErrors = (
+  body: Record<"errors", GraphQLFormattedError[] | undefined>
+): Error | null | undefined => {
+  if (!body) {
+    return {
+      message: "Unknown error",
+      statusCode: "INTERNAL_SERVER_ERROR",
+    };
+  }
+
+  if ("errors" in body && body.errors) {
+    const errors = body.errors;
+    const messages = errors.map((error) => error.message).join("");
+    const code = errors.[0].extensions.code;
+
+    return {
+      message: messages || JSON.stringify(errors),
+      statusCode: code || 500
+    }
+  }
+
+  return null
 };
--- src/providers/data/index.tsx
+++ src/providers/data/index.tsx
@@ -1,4 +1,6 @@
 import { GraphQLClient } from "@refinedev/nestjs-query";
+import { customFetch, getGraphQLErrors } from "./fetch-wrapper";
+
 export const API_URL = "https://api.crm.refine.dev";
 
 export const client = new GraphQLClient(API_URL, {
@@ -10,3 +12,17 @@ export const client = new GraphQLClient(API_URL, {
     }
   },
 });
+
+const fetchWrapper = async (url: string, options: RequestInit) => {
+  const response = await customFetch(url, options);
+  /* clone() help us to consume the response more than once time */
+  const responseClone = response.clone();
+  const body = await responseClone.json();
+  const error = getGraphQLErrors(body);
+
+  if (error) {
+    throw error;
+  }
+
+  return response;
+};
